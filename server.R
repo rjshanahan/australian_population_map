b44126ac77f4937c03ee558b709b18e9cbee3ff8
Richard Shanahan <rjshanahan@gmail.com>
2017-07-04 13:00:30 +0930

    change map zoom

--- server.R
+++ server.R
@@ -62,107 +62,13 @@ server <- function(input,output,sessions) {
         urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
         attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
       ) %>%
-      setView(lng = 150.0000, lat = -24.1500, zoom = 4)
+      setView(lng = 135.0000, lat = -24.1500, zoom = 4)
     
     #if statement to determine which sentiment analysis metrics will be shown
-    if (population_type == "2016 population by area") {
+    if (population_type == "2016 vs 2006 population change") {
       
       # define bins
-      bins <- c(0, 1000, 5000, 6000, 7000, 8000, 9000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, Inf)
-      
-      # construct labels
-      labels <- sprintf(
-        "<strong>%s</strong><br/>%s - population in 2016 <br/><ul><li>%s - population in 2006 </li><li>%s percent change in population from 2006 to 2016</li></ul>",
-        simp_myAus$SA2_NAM, 
-        simp_myAus$population_2016,
-        simp_myAus$population_2006,
-        simp_myAus$population_change
-      ) %>% lapply(htmltools::HTML)
-      
-      # colour pallette
-      pal <- colorBin("YlOrRd", 
-                      domain = simp_myAus$population_2016 , 
-                      bins = bins)
-      
-    theMap %>%
-      addPolygons(
-        fillColor = ~pal(population_2016),
-        weight = 2,
-        opacity = 1,
-        color = "white",
-        dashArray = "3",
-        fillOpacity = 0.7,
-        highlight = highlightOptions(
-          weight = 5,
-          color = "#666",
-          dashArray = "",
-          fillOpacity = 0.7,
-          bringToFront = TRUE),
-        label = labels,
-        labelOptions = labelOptions(
-          style = list("font-weight" = "normal", padding = "3px 8px"),
-          textsize = "15px",
-          direction = "auto"))  %>%
-      addLegend(pal = pal, values = ~population_2016, opacity = 0.7, title = NULL,
-                position = "bottomright") %>%
-      addMeasure(
-        position = "bottomleft",
-        primaryLengthUnit = "meters",
-        primaryAreaUnit = "sqmeters",
-        activeColor = "#3D535D",
-        completedColor = "#7D4479")
-
-    } else if (population_type == "2006 population by area") {
-      
-      # define bins
-      bins <- c(0, 1000, 5000, 6000, 7000, 8000, 9000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, Inf)
-      
-      # construct labels
-      labels <- sprintf(
-        "<strong>%s</strong><br/>%s - population in 2006 <br/><ul><li>%s - population in 2016 </li><li>%s percent change in population from 2006 to 2016</li></ul>",
-        simp_myAus$SA2_NAM, 
-        simp_myAus$population_2006,
-        simp_myAus$population_2016,
-        simp_myAus$population_change
-      ) %>% lapply(htmltools::HTML)
-      
-      # colour pallette
-      pal <- colorBin("YlOrRd", 
-                      domain = simp_myAus$population_2006 , 
-                      bins = bins)
-      
-      theMap %>%
-        addPolygons(
-          fillColor = ~pal(population_2006),
-          weight = 2,
-          opacity = 1,
-          color = "white",
-          dashArray = "3",
-          fillOpacity = 0.7,
-          highlight = highlightOptions(
-            weight = 5,
-            color = "#666",
-            dashArray = "",
-            fillOpacity = 0.7,
-            bringToFront = TRUE),
-          label = labels,
-          labelOptions = labelOptions(
-            style = list("font-weight" = "normal", padding = "3px 8px"),
-            textsize = "15px",
-            direction = "auto"))  %>%
-        addLegend(pal = pal, values = ~population_2006, opacity = 0.7, title = NULL,
-                  position = "bottomright") %>%
-        addMeasure(
-          position = "bottomleft",
-          primaryLengthUnit = "meters",
-          primaryAreaUnit = "sqmeters",
-          activeColor = "#3D535D",
-          completedColor = "#7D4479")
-      
-    } else if (population_type == "2016 vs 2006 population change") {
-      
-      # define bins
-      bins <- c(-Inf, -20, -10, -5, -1, -0.5, 0, 1, 5, 7, 9, 10, 15, 20, 25, 30, 50, Inf)
+      bins <- c(-Inf, -20, -10, -5, -1, 0, 1, 5, 9, 10, 15, 20, 25, 30, 50, 100, Inf)
       
       # construct labels
       labels <- sprintf(
@@ -198,6 +104,100 @@ server <- function(input,output,sessions) {
             textsize = "15px",
             direction = "auto"))  %>%
         addLegend(pal = pal, values = ~population_change, opacity = 0.7, title = NULL,
+                  position = "bottomright") %>%
+        addMeasure(
+          position = "bottomleft",
+          primaryLengthUnit = "meters",
+          primaryAreaUnit = "sqmeters",
+          activeColor = "#3D535D",
+          completedColor = "#7D4479")
+      
+    } else if (population_type == "2016 population by area") {
+      
+      # define bins
+      bins <- c(0, 1000, 5000, 7500, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, Inf)
+      
+      # construct labels
+      labels <- sprintf(
+        "<strong>%s</strong><br/>%s - population in 2016 <br/><ul><li>%s - population in 2006 </li><li>%s percent change in population from 2006 to 2016</li></ul>",
+        simp_myAus$SA2_NAM, 
+        simp_myAus$population_2016,
+        simp_myAus$population_2006,
+        simp_myAus$population_change
+      ) %>% lapply(htmltools::HTML)
+      
+      # colour pallette
+      pal <- colorBin("Oranges", 
+                      domain = simp_myAus$population_2016 , 
+                      bins = bins)
+      
+    theMap %>%
+      addPolygons(
+        fillColor = ~pal(population_2016),
+        weight = 2,
+        opacity = 1,
+        color = "white",
+        dashArray = "3",
+        fillOpacity = 0.7,
+        highlight = highlightOptions(
+          weight = 5,
+          color = "#666",
+          dashArray = "",
+          fillOpacity = 0.7,
+          bringToFront = TRUE),
+        label = labels,
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto"))  %>%
+      addLegend(pal = pal, values = ~population_2016, opacity = 0.7, title = NULL,
+                position = "bottomright") %>%
+      addMeasure(
+        position = "bottomleft",
+        primaryLengthUnit = "meters",
+        primaryAreaUnit = "sqmeters",
+        activeColor = "#3D535D",
+        completedColor = "#7D4479")
+
+    } else if (population_type == "2006 population by area") {
+      
+      # define bins
+      bins <- c(0, 1000, 5000, 7500, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, Inf)
+      
+      # construct labels
+      labels <- sprintf(
+        "<strong>%s</strong><br/>%s - population in 2006 <br/><ul><li>%s - population in 2016 </li><li>%s percent change in population from 2006 to 2016</li></ul>",
+        simp_myAus$SA2_NAM, 
+        simp_myAus$population_2006,
+        simp_myAus$population_2016,
+        simp_myAus$population_change
+      ) %>% lapply(htmltools::HTML)
+      
+      # colour pallette
+      pal <- colorBin("Oranges", 
+                      domain = simp_myAus$population_2006 , 
+                      bins = bins)
+      
+      theMap %>%
+        addPolygons(
+          fillColor = ~pal(population_2006),
+          weight = 2,
+          opacity = 1,
+          color = "white",
+          dashArray = "3",
+          fillOpacity = 0.7,
+          highlight = highlightOptions(
+            weight = 5,
+            color = "#666",
+            dashArray = "",
+            fillOpacity = 0.7,
+            bringToFront = TRUE),
+          label = labels,
+          labelOptions = labelOptions(
+            style = list("font-weight" = "normal", padding = "3px 8px"),
+            textsize = "15px",
+            direction = "auto"))  %>%
+        addLegend(pal = pal, values = ~population_2006, opacity = 0.7, title = NULL,
                   position = "bottomright") %>%
         addMeasure(
           position = "bottomleft",
